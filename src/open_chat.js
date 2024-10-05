07f8f4c23f6fc8224151a9aab5289e12cac866de
AndreMoreira00 <andremoreira102030@gmail.com>
2024-10-04 23:00:25 -0300

    x

--- src/open_chat.js
+++ src/open_chat.js
@@ -1,9 +1,13 @@
 const bt = document.getElementById('bd');
 const chat = document.getElementById('chat');
 const image_bt = document.getElementById('bd_image');
+const bd = document.getElementById('x');
 
 function visible(){
   const visibility = window.getComputedStyle(chat, null).visibility;
+  
+  var screenHeight = screen.height;
+
   if (visibility == 'hidden'){
     chat.style.visibility = 'visible';
     image_bt.src = "images/seta.png";
@@ -11,6 +15,20 @@ function visible(){
     chat.style.visibility = 'hidden';
     image_bt.src = "images/icon.svg";
   }
+
+  if(screenHeight <= 479){
+    bt.style.visibility = 'hidden';
+    image_bt.src = "images/icon.svg";
+    bd.style.visibility = 'visible'
+  }
 }
 
+function close_chat(){
+  chat.style.visibility = 'hidden';
+  image_bt.src = "images/icon.svg";
+  bt.style.visibility = 'visible';
+  bd.style.visibility = 'hidden';
+}
+
+bd.onclick = function(){close_chat()};
 bt.onclick = function(){visible()};
